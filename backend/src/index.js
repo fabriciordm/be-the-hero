8ddebe0a03516f5b2a6cb3d5f52362afd1c68a0f
Fabricio Rodrigues de Moura <fabriciomoura.bhs@fundep.local>
2020-03-25 12:06:59 -0300

    Back-end of Application

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes')
+
+const app = express();
+
+app.use(cors);
+app.use(express.json());
+app.use(routes);
+
+/**
+ * Tipos de parâmetros
+ * 
+ * Query params:ex /users?name=Jennifer&redSocial=Tinder
+ * Route params: users/1 como se fosse where no sql 
+ * Request Body:
+ * 
+ * 
+ */
+
+
+
+
+app.listen(3333);
---
+++ backend/src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const routes = express.Router();
+const OngController = require('../src/Controllers/OngController')
+const IncidentController = require('../src/Controllers/IncidentController')
+const ProfileController = require('../src/Controllers/ProfileController')
+const SessionController = require('../src/Controllers/SessionController')
+
+
+
+
+routes.get('/ongs', OngController.index);
+routes.get('/profile',ProfileController.index);
+routes.post('/sessions',SessionController.create);
+routes.post('/ongs',OngController.create);
+routes.post('/incidents',IncidentController.create);
+routes.get('/incidents',IncidentController.index);
+routes.delete('/incidents/:id',IncidentController.delete);
+
+
+
+module.exports= routes;
